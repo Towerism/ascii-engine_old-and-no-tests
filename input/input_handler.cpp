221d80dde9316464f5c7e8bebe3b623b5a3f9ec8
Martin Fracker <martin.frackerjr@gmail.com>
2015-03-30 02:52:34 -0500

    Changed vector to map

--- input/input_handler.cpp
+++ input/input_handler.cpp
@@ -13,24 +13,24 @@ ae::Input_handler::Input_handler() {
 }
 
 void ae::Input_handler::poll() {
-    char c;
+    int c;
     // clear keys unused from last poll
-    input_vec.clear();
+    input_map.clear();
     while(true) {
         c = getch();
         if (c == ERR) {
             break;
         }
-        input_vec.push_back(c);
+        input_map.insert(std::make_pair(c, c));
     }
 }
 
 bool ae::Input_handler::check_key(int key) {
-    const auto& it = find_if(input_vec.begin(), input_vec.end(),
-        [key](int c) { return c == key; }
+    const auto& it = find_if(input_map.begin(), input_map.end(),
+        [key](const std::pair<int, int>& elem) { return elem.first == key; }
     );
-    if (it != input_vec.end()) {
-        input_vec.erase(it);
+    if (it != input_map.end()) {
+        input_map.erase(it);
         return true;
     }
     return false;
