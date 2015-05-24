1872b82c809c59429778254bbd5c610f20de42fa
Martin Fracker <martin.frackerjr@gmail.com>
2015-05-24 16:12:37 -0500

    Refactored and added source file for framerate_limiter

---
+++ timing/framerate_limiter.cpp
@@ -0,0 +1,43 @@
+/**********************************
+ * author: Martin Fracker, Jr.
+ * license: GNU GPL
+ * * * * * * * * * * * * * * * * *
+ * file: framerate_limiter.h
+ * library: ascii-engine
+ **********************************/
+
+#include "framerate_limiter.h"
+
+namespace ae = ascii_engine;
+
+void ae::Framerate_limiter::frame_start() {
+  start = clock.now();
+}
+
+void ae::Framerate_limiter::frame_end() {
+  end_frame_clock();
+  if (there_is_residual_time()) {
+    sleep_for_remaining_time();
+  } else {
+    end_frame_immediately();
+  }
+}
+
+void ae::Framerate_limiter::end_frame_clock() {
+  end = std::chrono::time_point_cast<ms>(clock.now());
+  frame_time = end - start;
+}
+
+bool ae::Framerate_limiter::there_is_residual_time() {
+  return frame_time.count() < target_dur.count();
+}
+
+void ae::Framerate_limiter::sleep_for_remaining_time() {
+  ms sleep_length = std::chrono::duration_cast<ms>(target_dur - frame_time);
+  msleep(sleep_length.count());
+  delta_time = frame_time + sleep_length;
+}
+
+void ae::Framerate_limiter::end_frame_immediately() {
+  delta_time = frame_time;
+}
--- timing/framerate_limiter.h
+++ timing/framerate_limiter.h
@@ -25,33 +25,34 @@ void msleep(unsigned milliseconds) {
 #endif
 
 namespace ascii_engine {
+
   struct Framerate_limiter {
+
     typedef std::chrono::milliseconds ms;
     typedef std::chrono::duration<double> sec;
     typedef std::chrono::steady_clock steady_clock;
 
-    Framerate_limiter(int target) : 
+    Framerate_limiter(int target) :
       target_fps(target), target_dur(steady_clock::period::den/target) { }
-    void frame_start() {
-      start = clock.now();
+
+    void frame_start();
+    void frame_end();
+
+    double get_delta_time() {
+      return std::chrono::duration_cast<sec>(delta_time).count();
     }
-    void frame_end() {
-      end = std::chrono::time_point_cast<ms>(clock.now());
-      frame_time = end - start;
-      if (frame_time.count() < target_dur.count()) {
-        ms sleep_length = std::chrono::duration_cast<ms>(target_dur - frame_time);
-        msleep(sleep_length.count());
-        delta_time = frame_time + sleep_length;
-      } else {
-        delta_time = frame_time;
-      }
-    }
-    double get_delta_time() { return std::chrono::duration_cast<sec>(delta_time).count(); }
+
   private:
+
     steady_clock clock;
     int target_fps;
     steady_clock::duration target_dur, frame_time, delta_time;
     steady_clock::time_point start, end;
+
+    void end_frame_clock();
+    bool there_is_residual_time();
+    void sleep_for_remaining_time();
+    void end_frame_immediately();
   };
 }
 
