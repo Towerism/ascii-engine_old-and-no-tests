df9b2a20b94c4c539a2170b4f37144e8e2979a4f
Martin Fracker <martin.frackerjr@gmail.com>
2015-06-01 00:00:36 +0000

    Moved public headers into one header

--- engine.cpp
+++ engine.cpp
@@ -16,7 +16,7 @@ using namespace std;
 void ae::Engine::game_loop() {
   using Input_handler = ae::Input_handler;
   Framerate_limiter fps_limiter(target_fps);
-  Input_handler& input = Input_handler::get_instance();
+  Input_handler& input = Input_handler::get();
   in_loop = true;
   while (!exit_loop) {
     fps_limiter.frame_start();
@@ -28,7 +28,7 @@ void ae::Engine::game_loop() {
 }
 
 void ae::Engine::poll_exit_key() {
-  if (ae::Input_handler::get_instance().check_key(exit_key)) {
+  if (ae::Input_handler::get().check_key(exit_key)) {
     terminate_loop();
   }
 }
---
+++ include/ascii_engine.h
@@ -0,0 +1,9 @@
+#ifndef ASCII_ENGINE_H
+#define ASCII_ENGINE_H
+
+#include <engine.h>
+#include <entity.h>
+#include <world.h>
+#include <input/input_handler.h>
+
+#endif // ASCII_ENGINE_H
