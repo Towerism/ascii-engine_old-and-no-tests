8aadec58e7c2a1b69bd286ee63702287f15eb3c0
Martin Fracker <martin.frackerjr@gmail.com>
2015-06-01 00:26:10 +0000

    Changed paths to look in include/internal

--- include/ascii_engine.h
+++ include/ascii_engine.h
@@ -1,9 +1,9 @@
 #ifndef ASCII_ENGINE_H
 #define ASCII_ENGINE_H
 
-#include <engine.h>
-#include <entity.h>
-#include <world.h>
-#include <input/input_handler.h>
+#include <internal/engine.h>
+#include <internal/entity.h>
+#include <internal/world.h>
+#include <internal/input_handler.h>
 
 #endif // ASCII_ENGINE_H
