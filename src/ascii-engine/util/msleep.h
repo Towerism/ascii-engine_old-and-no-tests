f33d6babeaa02314123a7ec1b32ef856c9b2a3e6
Martin Fracker <martin.frackerjr@gmail.com>
2015-06-02 02:30:53 +0000

    Moved msleep into ascii_engine namespace

--- src/ascii-engine/util/msleep.h
+++ src/ascii-engine/util/msleep.h
@@ -1,17 +1,25 @@
 #ifndef MSLEEP_H
 #define MSLEEP_H
 
-// cross-platform millisecond sleep function
+namespace ascii_engine {
+
+  // cross-platform millisecond sleep function
 #ifdef _WIN32
 #include <windows.h>
-void msleep(unsigned milliseconds) {
-  Sleep(milliseconds);
-}
+
+  void msleep(unsigned milliseconds) {
+    Sleep(milliseconds);
+  }
+
 #else
 #include <unistd.h>
-void msleep(unsigned milliseconds) {
-  usleep(milliseconds * 1000); // takes microseconds
-}
+
+  void msleep(unsigned milliseconds) {
+    usleep(milliseconds * 1000); // takes microseconds
+  }
+
 #endif
+
+}
 
 #endif // MSLEEP_H
